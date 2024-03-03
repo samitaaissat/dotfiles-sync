5c481a3b036a06ee20a634f00922c693903d1fa0
Sami TAAISSAT <sami.taaissat@artefact.com>
2024-03-03 11:04:18 +0100

    feat: start working on cli

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 mod tpl_io;
 
+use clickrs::command;
 use handlebars::Handlebars;
+use std::path::PathBuf;
 
-fn main() {
-    let templates = tpl_io::load_tpl::load_templates("tests/assets").unwrap();
+#[command(
+    name = "Dotfiles Sync",
+    about = "A utility to easily sync configuration files easily."
+)]
+#[argument(
+    "input",
+    help = "Folder where config files are located.",
+    default_value = ".",
+    parse(from_os_str)
+)]
+#[argument(
+    "output",
+    help = "Folder where config files will be placed.",
+    default_value = "~",
+    parse(from_os_str)
+)]
+fn main(input: PathBuf, output: PathBuf) {
+    let templates = tpl_io::load_tpl::load_templates(input.to_str().unwrap()).unwrap();
     let mut reg = Handlebars::new();
 
     for template in templates {
